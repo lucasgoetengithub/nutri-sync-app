81aee3d1fc9b6daa2c8be3c155b6cb7ed942f893
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-01-23 17:39:54 -0300

    iniciando login

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Login from '../views/Login';
+
+const Private = ({ Item }) => {
+    // const user = JSON.parse(localStorage.getItem("user_token"));    
+
+    // const { signed } = useAuth();
+    // var logado = false;
+    // if (user != null) {
+    //     logado = true;
+    // } else {
+    //     logado = signed;
+    // }
+    
+    // return logado ? <Item/> : <Login/>
+    return true;
+}
+
+export default function Routers() {
+    return (
+        <BrowserRouter>
+        <Fragment>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                </Routes>
+        </Fragment>
+        </BrowserRouter>
+    )
+}
---
+++ src/views/Login/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, Component } from 'react'
+import styled from "styled-components";
+import * as S from './styles';
+import { Link } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
+
+
+const Login = () => {
+
+    useEffect(() =>{
+      
+    });
+    
+      
+    return (
+        <S.Container>
+            
+        </S.Container>
+    )
+  
+  }
+  
+  export default Login;
