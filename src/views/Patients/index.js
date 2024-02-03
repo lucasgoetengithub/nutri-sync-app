8887bd2bdf3798dd4b5055bdea085a80683e98ca
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-02-03 00:52:58 -0300

    tela pacientes

--- src/views/Patients/index.js
+++ src/views/Patients/index.js
@@ -6,9 +6,72 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import logoImage from '../../assets/logo.jpg';
 import api from '../../service/api';
+// import { DataGrid, GridColumns, GridEventListener, GridCellParams } from '@mui/x-data-grid';
+// import { StyledEngineProvider } from '@mui/material/styles';
 
+// const columns: GridColumns = [
+//     { 
+//         field: "description", 
+//         headerName: "Alocação", 
+//         width: 180, 
+//         editable: true 
+//     },
+//     { 
+//         field: "percentage", 
+//         headerName: "Porcentagem %", 
+//         type: "number", 
+//         width: 180, 
+//         editable: true 
+//     },
+//     {
+//       field: "maxAmount",
+//       headerName: "Quantidade",
+//       type: "number",
+//       width: 150,
+//       editable: true
+//     }
+// ];
 
 const Patients = () => {
+
+    const [rows, setRows] = useState([]);
+    const [name, setName] = useState([]);
+
+    const handleRowClick: GridEventListener<'rowClick'> = (params) => {
+        // setFocusedInvest(params.row.id);
+        // setFocusedInvestMaxAmount(params.row.maxAmount);
+
+        // api.get(`/DistributionWhereInvest/` + whereInvestId, { 'headers': { 'Authorization': token } })
+        // .then(response=> {
+        //     var rowsAllocation = [];
+        //     var totalUsado = 0;
+        //     if (response.data.jsonDistributionWhereInvests) {
+        //         response.data.jsonDistributionWhereInvests.forEach(element => {
+        //             if (element.idWhereInvestAllocation === params.row.id) {
+        //                 totalUsado = totalUsado + element.amountUsed;
+        //                 rowsAllocation.push({
+        //                     id: element.id,
+        //                     description: element.description,
+        //                     amount: element.amount,
+        //                     amountUsed: element.amountUsed,
+        //                     investExpense: element.investExpense,
+        //                     idWhereInvestAllocation: element.idWhereInvestAllocation
+        //                   })
+        //             }
+                    
+        //         });
+        //     }
+
+        //     if (totalUsado > params.row.maxAmount) {
+        //         setMessageDistribution('Cuidado! Seus gastos estão excedendo os valores do seu planejamento para esta alocação ');
+        //     } else {
+        //         setMessageDistribution('');
+        //     }
+
+        //     setRowsAllocation(rowsAllocation);
+        // })
+        
+    };
 
     
     useEffect(() =>{
@@ -78,6 +141,51 @@ const Patients = () => {
 
                 </S.SidebarList>
             </S.Sidebar>
+                
+            <S.Painel1>
+                <Stack width='80%' spacing={3.8} direction="row">
+                    <S.StyledTextField
+                        required
+                        id="outlined-required"
+                        label="Nome"
+                        onChange={(e) => [setName(e.target.value)]}
+                    />
+
+                    <S.StyledTextField
+                        required
+                        id="outlined-required"
+                        label="Outro Campo"
+                        onChange={(e) => [setName(e.target.value)]}
+                    />
+
+                    <Button variant="primary" type="submit" onClick={handleSignin}>Buscar</Button>
+
+                </Stack>
+            </S.Painel1>
+
+            <S.Painel2>
+                <S.DataGridContainer>
+                    <thead>
+                        <S.TableRow>
+                            <S.TableHeader width="800px">Nome</S.TableHeader>
+                            <S.TableHeader>Data de Nascimento</S.TableHeader>
+                            <S.TableHeader>CPF</S.TableHeader>
+                            <S.TableHeader>Telefone</S.TableHeader>
+                        </S.TableRow>
+                    </thead>
+                    <tbody>
+                        {rows.map((item, index) => (
+                        <S.TableRow key={index} even={index % 2 === 0}>
+                            <S.TableCell width="800px">{item.nome}</S.TableCell>
+                            <S.TableCell >{item.dataNascimento}</S.TableCell>
+                            <S.TableCell >{item.cpf}</S.TableCell>
+                            <S.TableCell >{item.telefone}</S.TableCell>
+                        </S.TableRow>
+                        ))}
+                    </tbody>
+                </S.DataGridContainer>
+            </S.Painel2>
+            
         </>
     )
   
--- src/views/Patients/styles.js
+++ src/views/Patients/styles.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import TextField from '@mui/material/TextField';
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -40,3 +41,59 @@ export const Logo = styled.img`
   width: 50%;
   margin-bottom: 10px;
 `
+
+export const Painel1 = styled.div`
+  background-color: #ffffff;
+  border-radius: 8px;
+  width: 70%;
+  margin: 50px auto;
+  margin-left: 450px;
+  position: relative;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+`;
+
+export const Painel2 = styled.div`
+  background-color: #ffffff;
+  border-radius: 8px;
+  width: 70%;
+  margin: 150px auto;
+  margin-left: 450px;
+  position: relative;
+  height: 200px;
+`;
+
+export const DataGridContainer = styled.div`
+  width: 100%;
+  margin: 50px;
+  border-collapse: collapse;
+  
+`;
+
+export const TableHeader = styled.th`
+  border: 1px solid #ddd;
+  padding: 12px;
+  text-align: left;
+  background-color: transparent;
+  border: none;
+`;
+
+export const TableRow = styled.tr`
+    &:nth-child(even) {
+      background-color: ${(props) => (props.even ? '#ffffff' : '#f2f2f2')};
+    }
+`;
+
+export const TableCell = styled.td`
+  border: none;
+  padding: 12px;
+  
+`;
+
+export const StyledTextField = styled(TextField)`
+  width: 500px;  // Ajuste o valor conforme necessário
+  margin-right: 20px;  // Ajuste o valor conforme necessário
+`;
