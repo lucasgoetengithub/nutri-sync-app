8eb42ffc6568c81244001ce78c233513f7841dc8
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-02-05 18:40:02 -0300

    salvar paciente

--- src/views/NewPatient/index.js
+++ src/views/NewPatient/index.js
@@ -5,6 +5,9 @@ import { Stack } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { Button } from 'react-bootstrap';
 import InputMask from 'react-input-mask';
+import api from '../../service/api';
+import { Link, useNavigate } from "react-router-dom";
+
 
 
 const NewPatient = () => {
@@ -22,10 +25,65 @@ const NewPatient = () => {
     const [gender, setGender] = useState("");
     const [cidade, setCidade] = useState("");
     const [cargo, setCargo] = useState("");
-     
+    const [observation, setObservation] = useState("");
+    const navigate = useNavigate();
+
+
     useEffect(() =>{
       
     });
+
+    function generateUserName(name) {
+        const words = name.split(' ');
+      
+        if (words.length === 1) {
+          return words[0].toLowerCase();
+        }
+      
+        const firstLetter = words[0][0].toLowerCase();
+        const lastWord = words[words.length - 1].toLowerCase();
+      
+        return `${firstLetter}${lastWord}`;
+    }
+
+    function generatePassword(dateOfBirth) {
+        const numericDateOfBirth = dateOfBirth.replace(/\D/g, '');
+        return numericDateOfBirth;
+    }
+
+    function convertDateFormat(inputDate) {
+        const [day, month, year] = inputDate.split('/');
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+      }
+
+
+    const handleRegister = async (event) => {
+        let usernamevar = generateUserName(name);
+        let passwordvar = generatePassword(dateBirth);
+        let datevar = convertDateFormat(dateBirth);
+
+        await api.post(`/api/patients/`, {
+            "username": usernamevar,
+            "name": name,
+            "email": email,
+            "password": passwordvar,
+            "age": age,
+            "dateOfBirth": datevar,
+            "address": address,
+            "city": cidade,
+            "occupation": cargo,
+            "obs": observation,
+            "phoneContact": contato,
+            "gender": gender
+        })
+        .then(response => {
+            navigate('/home')
+        })
+        .catch(err => {
+            
+        })    
+    }
 
 
     return (
@@ -80,11 +138,11 @@ const NewPatient = () => {
                         <S.Field>
                             <S.Label><h5>Contato</h5></S.Label>
                             <InputMask
-                                mask="(00) 00000-0000"
-                                label="(00) 00000-0000"
-                                maskPlaceholder="_"
-                                value={dateBirth}
-                                onChange={(e) => [setDateBirth(e.target.value), setError("")]}
+                                mask="(99) 99999-9999"
+                                label="(99) 99999-9999"
+                                maskChar="_"
+                                value={contato}
+                                onChange={(e) => setContato(e.target.value)}
                             >
                                 {(inputProps) => <TextField {...inputProps} sx={{ width: '200px' }} />}
                             </InputMask>
@@ -158,8 +216,22 @@ const NewPatient = () => {
                         />
                     </S.Field>
 
+                    <Stack width='600px' spacing={5} direction="row" margin='20px 10px 10px 40px'>
                     <S.Field>
-                        <Button variant="primary" type="submit" >Cadastrar</Button>
+                        <S.Label><h5>Observação</h5></S.Label>
+                        <TextField
+                        multiline
+                        rows={9} // você pode ajustar o número de linhas conforme necessário
+                        id="outlined-multiline"
+                        label="Observação"
+                        onChange={(e) => [setObservation(e.target.value), setError("")]}
+                        sx={{ width: '800px' }}
+                        />
+                    </S.Field>
+                    </Stack>
+
+                    <S.Field>
+                        <Button variant="primary" type="submit" onClick={handleRegister}>Cadastrar</Button>
                     </S.Field>
                     
                 </Stack>
