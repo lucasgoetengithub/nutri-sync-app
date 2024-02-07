eda79b1a2608974a5ed2955c25ea428fab196f94
Lucas Goeten <lucasgoetengithub@gmail.com>
2024-02-07 14:35:06 -0300

    criando tab menu

--- src/views/PatientData/index.js
+++ src/views/PatientData/index.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './styles';
 import SidebarHome from '../../components/SidebarHome';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Box from '@mui/material/Box';
+import styled, { createGlobalStyle } from 'styled-components';
 
 
 const PatientData = () => {
 
-    const [tabIndex, setTabIndex] = useState(0);
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState('1');
+    const [panelHeight, setPanelHeight] = useState(800); 
 
-    const handleTabChange = (index) => {
-      setTabIndex(index);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+
+        if (newValue === '1') {
+            setPanelHeight(800); 
+        } else if (newValue === '2') {
+            setPanelHeight(1000); 
+        } else if (newValue === '3') {
+            setPanelHeight(1200); 
+        }
     };
 
+    
     
     useEffect(() =>{
       
@@ -24,26 +39,30 @@ const PatientData = () => {
         <>
             <S.GlobalStyle />
             <SidebarHome></SidebarHome>       
-            <S.Painel1>
-                <Tabs selectedIndex={tabIndex} onSelect={handleTabChange} width={'100%'}>
-                    <TabList>
-                        <Tab>Dados Pessoais</Tab>
-                        <Tab>Medidas</Tab>
-                        <Tab>Bioimpedancia</Tab>
-                    </TabList>
+            <S.Painel1 style={{ height: panelHeight }}>
+                <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <S.StyledTab label="Item One" value="1" style={{ color: value === '1' ? '#B66DFF' : '' }}/>
+                                <S.StyledTab label="Item Two" value="2" style={{ color: value === '2' ? '#B66DFF' : '' }}/>
+                                <S.StyledTab label="Item Three" value="3" style={{ color: value === '3' ? '#B66DFF' : '' }}/>
+                            </TabList>
+                        </Box>
 
-                    <TabPanel>
-                        <h1>Teste 1</h1>
-                    </TabPanel>
-                    
-                    <TabPanel>
-                        <h1>Teste 2</h1>
-                    </TabPanel>
-
-                    <TabPanel>
-                        <h1>Teste 3</h1>
-                    </TabPanel>
-                </Tabs>
+                        <TabPanel value="1">
+                            Item One
+                        </TabPanel>
+                        
+                        <TabPanel value="2">
+                            Item Two
+                        </TabPanel>
+                        
+                        <TabPanel value="3">
+                            Item Three
+                        </TabPanel>
+                    </TabContext>
+                </Box>
             </S.Painel1>     
         </>
     )
--- src/views/PatientData/styles.js
+++ src/views/PatientData/styles.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import Tab from '@mui/material/Tab';
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -12,11 +13,18 @@ export const GlobalStyle = createGlobalStyle`
 export const Painel1 = styled.div`
   background-color: #ffffff;
   border-radius: 8px;
-  width: 45%;
+  width: 60%;
   margin: 90px auto;
   margin-left: 30%;
   position: relative;
   height: 800px;
   display: flex;
   justify-content: center;
+`;
+
+
+export const StyledTab = styled(Tab)`
+&.Mui-selected {
+    border-bottom: 2px solid #B66DFF;
+}
 `;
